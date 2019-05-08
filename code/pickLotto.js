3f3116c9d823e865fbf74d28e8a2adfa198d83f3
hoik92 <hoik92@nate.com>
2019-05-08 10:49:44 +0900

    원하는 갯수만큼 로또번호 추천하는 기능 구현

--- code/pickLotto.js
+++ code/pickLotto.js
@@ -1,18 +1,22 @@
-module.exports.function = function pickLotto () {
+module.exports.function = function pickLotto (numberOfPick) {
   let result = ''
   let numbers = []
   let n = 0
-  while (numbers.length < 6) {
+  let iteration = 6
+  if (numberOfPick) {
+    iteration = numberOfPick
+    if (iteration > 6) {
+      return '로또 번호는 6개입니다.'
+    }
+  }
+  while (numbers.length < iteration) {
     n = Math.ceil(Math.random() * 45)
     if (numbers.indexOf(n) < 0) {
       numbers.push(n)
-      // result += n.toString() + ' '
-    }
+   }
   }
   function compare (a, b) {return a - b} 
   numbers = numbers.sort(compare)
-  numbers.forEach(number => {
-    result += number.toString() + ' '
-  })
+  result = numbers.join(', ')
   return result
 }
