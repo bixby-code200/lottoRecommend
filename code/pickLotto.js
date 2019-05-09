5a145a0a450500257b7405e1ccb082c9f8b5a9f5
hoik92 <hoik92@nate.com>
2019-05-09 13:28:34 +0900

    트레이닝 추가

--- code/pickLotto.js
+++ code/pickLotto.js
@@ -1,22 +1,33 @@
-module.exports.function = function pickLotto (numberOfPick) {
+module.exports.function = function pickLotto (numberOfPick, addNumber) {
+  const console = require('console')
   let result = ''
   let numbers = []
   let n = 0
   let iteration = 6
+// numberOfPick이 들어오면 iteration을 numberOfPick으로 설정
   if (numberOfPick) {
     iteration = numberOfPick
     if (iteration > 6) {
       return '로또 번호는 6개입니다.'
     }
   }
+  
+  if (addNumber) {
+    addNumber.forEach(num => {
+      numbers.push(num)
+    })
+  }
+  
   while (numbers.length < iteration) {
     n = Math.ceil(Math.random() * 45)
     if (numbers.indexOf(n) < 0) {
       numbers.push(n)
    }
   }
+  
   function compare (a, b) {return a - b} 
   numbers = numbers.sort(compare)
   result = numbers.join(', ')
+  console.log(result)
   return result
 }
